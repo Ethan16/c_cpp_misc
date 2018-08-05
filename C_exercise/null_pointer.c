32c381af874c4aaa693b34e85cc4c1096eac3728
Ethan <euler52201044@sina.com>
2018-08-05 13:12:29 +0800

    Update null_pointer

--- C_exercise/null_pointer.c
+++ C_exercise/null_pointer.c
@@ -4,7 +4,7 @@
 @Author  : Ethan (euler52201044@163.com)
 @Link    : https://github.com/Ethan16
 @file    : null_pointer.c
-@usage   :
+@usage   :show null pointer
 @Version : 1.0
 @Change  : 2018-08-05 12:54:36
 """
